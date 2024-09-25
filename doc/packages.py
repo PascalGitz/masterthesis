7710c11dcadfde4338a0f600aa50d0c6abac2b4a
PascalGitz <pascal.gitz@hotmail.ch>
2024-09-25 14:15:54 +0200

    added plotstyle

--- doc/packages.py
+++ doc/packages.py
@@ -1,5 +1,15 @@
+
+## Pfad anpassen
+import os
+os.chdir(os.path.join(os.getcwd(),".."))
+
+
 from engicalc import *
 from engicalc.units import *
 import numpy as np
 import matplotlib.pyplot as plt 
 import sympy as sp
+
+#plotstyle
+from config.style.plotstyle import set_engineering_style  # Plotstyle
+set_engineering_style()
