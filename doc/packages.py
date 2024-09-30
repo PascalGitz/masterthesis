248ceff4c834f86d5e2c9fb33425d5bf6f01c1c0
PascalGitz <pascal.gitz@hotmail.ch>
2024-09-30 16:43:14 +0200

    format

--- doc/packages.py
+++ doc/packages.py
@@ -1,15 +1,16 @@
-
-## Pfad anpassen
+# Pfad anpassen
 import os
-os.chdir(os.path.join(os.getcwd(),".."))
 
+os.chdir(os.path.join(os.getcwd(), ".."))
 
+# Packages
 from engicalc import *
 from engicalc.units import *
 import numpy as np
-import matplotlib.pyplot as plt 
+import matplotlib.pyplot as plt
 import sympy as sp
 
-#plotstyle
+# plotstyle
 from config.style.plotstyle import set_engineering_style  # Plotstyle
+
 set_engineering_style()
