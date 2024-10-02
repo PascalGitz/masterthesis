54adebab2b40ee5ddd1af396344d1cfb21473830
PascalGitz <pascal.gitz@hotmail.ch>
2024-10-02 16:08:12 +0200

    upload

--- doc/packages.py
+++ doc/packages.py
@@ -9,6 +9,7 @@ from engicalc.units import *
 import numpy as np
 import matplotlib.pyplot as plt
 import sympy as sp
+from shapely.geometry import Polygon
 
 # plotstyle
 from config.style.plotstyle import set_engineering_style  # Plotstyle
